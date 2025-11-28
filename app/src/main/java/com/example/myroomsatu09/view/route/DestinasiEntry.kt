ea785d283fa726644055804724f1175f4b63445b
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-28 15:57:30 +0700

    Membuat dan Melengkapi DestinasiEntry.kt

---
+++ app/src/main/java/com/example/myroomsatu09/view/route/DestinasiEntry.kt
@@ -0,0 +1,10 @@
+package com.example.myroomsatu09.view.route
+
+
+import com.example.myroomsatu09.R
+import com.example.myroomsatu09.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
