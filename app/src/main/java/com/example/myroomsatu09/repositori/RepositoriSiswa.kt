ae2c38164ef0bc6ffac80d08eb3f898b390e5529
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-28 15:47:20 +0700

    Melengkapi repositori file RepositoriSiswa.kt

--- app/src/main/java/com/example/myroomsatu09/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu09/repositori/RepositoriSiswa.kt
@@ -1,1 +1,16 @@
 package com.example.myroomsatu09.repositori
+
+import com.example.myroomsatu09.room.Siswa
+import com.example.myroomsatu09.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+class OffLineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
