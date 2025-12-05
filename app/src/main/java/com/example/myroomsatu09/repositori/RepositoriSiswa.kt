2f8e526eb586712b096eb003a44d8880c8186eac
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-12-06 05:42:41 +0700

    Mengupdate RepositoriSiswa.kt

--- app/src/main/java/com/example/myroomsatu09/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu09/repositori/RepositoriSiswa.kt
@@ -7,10 +7,23 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+    //edit 1 : tambah 3 fungsi berikut
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa:Siswa)
+    //suspend fun updateSiswa(siswa:Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 class OffLineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-}
+
+    //edit 2: tambah 2ovverage berikut
+    override fun getSiswaStream(id : Int): Flow<Siswa?> =siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa:Siswa) =siswaDao.delete(siswa)
+    //override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+    }
+
