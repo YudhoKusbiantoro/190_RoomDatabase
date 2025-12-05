e48371e2a59ff7010255d8cd760f287491a95b4e
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-12-06 05:42:01 +0700

    Mengupdate PenyediaViewModel.kt

--- app/src/main/java/com/example/myroomsatu09/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu09/viewmodel/provider/PenyediaViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.myroomsatu09.viewmodel.provider
 
-import android.app.Application
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myroomsatu09.repositori.AplikasiSiswa
+import com.example.myroomsatu09.viewmodel.DetailViewModel
+import com.example.myroomsatu09.viewmodel.EditViewModel
 import com.example.myroomsatu09.viewmodel.EntryViewModel
 import com.example.myroomsatu09.viewmodel.HomeViewModel
 
@@ -14,16 +16,26 @@ object PenyediaViewModel {
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
-
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+       }
     }
-
-    /**
-     * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
-     * [AplikasiSiswa].
-     */
-    fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
-        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 }
+
+/**
+ * Fungsi esktensi query untuk objek [Application] dan mengembalikan sebuah instance dari [AplikasiSiswa]
+ */
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiSiswa)
