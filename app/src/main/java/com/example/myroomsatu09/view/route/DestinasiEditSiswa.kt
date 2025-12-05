f779e1be1bc8d1df00b2d88a007dd4865592716c
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-12-06 05:37:35 +0700

    Mengupdate DestinasiEditSiswa.kt

---
+++ app/src/main/java/com/example/myroomsatu09/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.myroomsatu09.view.route
+
+import com.example.myroomsatu09.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
